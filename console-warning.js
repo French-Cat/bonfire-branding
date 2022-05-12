9c634597d9f61d18186d332d0a369e0264f1ef61
FRCat.JS <89495053+French-Cat@users.noreply.github.com>
2022-05-12 16:11:39 -0400

    Update console-warning.js

--- console-warning.js
+++ console-warning.js
@@ -1,1 +1,1 @@
-console.log("%c| | ! Be careful in here ! | |", "padding: 0.3rem 1.5rem; font-size: 250%; line-height: 1.4em; color: white; background: red; border-radius: 8px;");
+console.log("%cWait, Don't do anything in here unless you know EXACTLY what you're doing!", "padding:0.3rem 1.5rem;font-size:250%;line-height:1.4em;color:white;background:red;border-radius:8px;");
